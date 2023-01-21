faff661e0a840ba82c46d4b42185f03db61c297b
OlgaPyryeva <vashkevicholga@gmail.com>
2023-01-21 19:55:37 +0300

    feat: check brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const openBrackets = ["(", "{", "[", "|", "1", "3", "5", "7", "8"];
+  let stack = [];
+  function checkBrackets(str) {
+    for (i = 0; i < str.length; i++) {
+      let symbol = str[i];
+      if (openBrackets.includes(symbol)) {
+        stack.push(symbol);
+      } else {
+        if (stack.length === 0) {
+          return false;
+        }
+
+        let lastSymbol = stack[stack.length - 1];
+        for (i = 0; i < bracketsConfig.length; i++) {
+          if (bracketsConfig[i][0] === lastSymbol) {
+            stack.pop();
+          } else {
+            return false;
+          }
+        }
+      }
+    }
+  }
+  checkBrackets(str);
+  return stack.length === 0;
+};
