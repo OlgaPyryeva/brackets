395a57040ac1fcee64028910bc9650cbcc6aa092
OlgaPyryeva <vashkevicholga@gmail.com>
2023-01-22 00:21:51 +0300

    fix: fix function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
   const openBrackets = ["(", "{", "[", "|", "1", "3", "5", "7", "8"];
+  const bracketsPair = {
+    [")"]: "(",
+    ["}"]: "{",
+    ["]"]: "[",
+    ["|"]: "|",
+    ["1"]: "2",
+    ["3"]: "4",
+    ["5"]: "6",
+    ["7"]: "7",
+    ["8"]: "8",
+  };
   let stack = [];
+
   function checkBrackets(str) {
     for (i = 0; i < str.length; i++) {
       let symbol = str[i];
@@ -12,16 +24,15 @@ module.exports = function check(str, bracketsConfig) {
         }
 
         let lastSymbol = stack[stack.length - 1];
-        for (i = 0; i < bracketsConfig.length; i++) {
-          if (bracketsConfig[i][0] === lastSymbol) {
-            stack.pop();
-          } else {
-            return false;
-          }
+
+        if (bracketsPair[symbol] === lastSymbol) {
+          stack.pop();
+        } else {
+          return false;
         }
       }
     }
+    return stack.length === 0;
   }
-  checkBrackets(str);
-  return stack.length === 0;
+  return checkBrackets(str);
 };
